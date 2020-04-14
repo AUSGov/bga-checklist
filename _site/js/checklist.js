ebc8923d598d1041601fdddcf4d9d0fb47e9b55f
blue-baron <bluebaron75@gmail.com>
2020-04-14 17:57:44 +1000

    Creating checklist pattern functionality.

--- _site/js/checklist.js
+++ _site/js/checklist.js
@@ -2,15 +2,51 @@
 
 $(document).ready(function () {
     
-   $(".checklist-item-title").on("click", function(){
-       console.log($(this).closest('.checklist-item'));
-       if ($(this).closest('.checklist-item').hasClass('open')) { 
-           $(this).closest('.checklist-item').removeClass('open');
-       } 
-       else {
-           $(this).closest('.checklist-item').addClass('open');
-       }
-   });
+    /*------------------- Open & close checklist items -------------------*/
+    $(".checklist-item-title").on("click", function () {
+        if ($(this).closest('.checklist-item').hasClass('open')) {
+            $(this).closest('.checklist-item').removeClass('open');
+        } else {
+            $(this).closest('.checklist-item').addClass('open');
+        }
+    });
+    
+    
+    /*------------------- Close checklist step button -------------------*/
+    $(' .checklist-close-step').on('click', function(){
+        $(this).parents('.checklist-item').removeClass('open');
+    });
+    
+    
+    /*------------------- Checkbox functionality -------------------*/
+    $('.checklist-item-checkbox').on('click', function(){
+        if ($(this).parents('.checklist-sub-item').hasClass('done')) {
+            $(this).parents('.checklist-sub-item').removeClass('done');
+        } else {
+            $(this).parents('.checklist-sub-item').addClass('done');
+        }
+        
+        $(this).parents('.checklist-item').find('.checklist-sub-item').each(function(){
+            
+            var item_completion = false;
+            if ($(this).hasClass('done')) {
+                item_completion = true;
+            } else {
+                item_completion = false;
+                $(this).parents('.checklist-item').removeClass('item-done');
+                return false;
+            }
+            if (item_completion === true) {
+                $(this).parents('.checklist-item').addClass('item-done');
+            }
+            
+        });
+    });
+    
+    
+    /*------------------- Email button functionality -------------------*/
+    
+
     
 }); // END doc ready
 
