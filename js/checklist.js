e9354500fa988c9e11cef921684e31afcef1d432
blue-baron <bluebaron75@gmail.com>
2020-04-19 17:55:40 +1000

    Updating button animation and substep functionlity.

--- js/checklist.js
+++ js/checklist.js
@@ -3,10 +3,9 @@
 $(document).ready(function () {
     
     /*------------------- Open & close checklist items -------------------*/
-     $(".checklist-item-title").on("click", function () {
+    $(".checklist-item-title").on("click", function () {
         $(this).next('.checklist-sub-item-wrapper').slideToggle(600);
 
-         
         if ($(this).closest('.checklist-item').hasClass('open')) {
             $(this).closest('.checklist-item').removeClass('open');
         } else {
@@ -20,6 +19,20 @@ $(document).ready(function () {
         $(this).parents('.checklist-sub-item-wrapper').hide();
         $(this).parents('.checklist-item').removeClass('open');
     });
+    
+    
+    /*------------------- Open & close sub-checklist items -------------------*/
+     $(".checklist-sub-item-title").on("click", function () {
+
+        $(this).next('.content-wrapper').slideToggle(600);
+         
+        if ($(this).closest('.checklist-sub-item').hasClass('open')) {
+            $(this).closest('.checklist-sub-item').removeClass('open');
+        } else {
+            $(this).closest('.checklist-sub-item').addClass('open');
+        }
+         
+     });
     
     
     /*------------------- Checkbox functionality -------------------*/
@@ -50,38 +63,58 @@ $(document).ready(function () {
     
     /*------------------- Email button functionality -------------------*/
     //Get position of button using the header height
-    var wrapper_pos = $('#email-wrapper').position(),
+    var wrapper_pos = $('.checklist-wrapper').offset(),
         wrapper_top = wrapper_pos.top,
-        btn_pos = $('.email-btn-wrapper').position(),
+        btn_pos = $('#email-btn-wrapper').offset(), //calculate so btn can return to original pos
         btn_left = btn_pos.left,
-        btn_left_minimised = btn_left + 80 + 194 + 56;
-       
-    console.log(btn_left);
+        btn_left_min = btn_left + 200 + 32; //left pos + btn width(200px) + gutter(32px)
         
-    $(window).scroll(function () {
-        if($(window).scrollTop() >= (wrapper_top)) {
-            $('#email-wrapper').addClass('minimised');
+     
+    console.log('Checklist top position: ', wrapper_top);
+    console.log('Button left position: ', btn_left);
+    console.log('Button left position - minimised: ', btn_left_min);
+
+    
+    $(window).scroll(function(){
+         if ($(window).scrollTop() > (wrapper_top)) {
+             $('#email-btn-wrapper').addClass('minimised');
+             $('.checklist-wrapper').addClass('scrolled');
+             
+             /*$('#email-btn-wrapper').css({
+                     //'transition': 'none',
+                     'top': '32px',
+                     'left': btn_left_min,
+                     'position': 'fixed',
+                 });*/
+             
+             setTimeout(function () {
+                 $('#email-btn-wrapper').css({
+                     'transition': 'none',
+                     'top': '32px',
+                     'left': btn_left_min,
+                     'position': 'fixed',
+                 });
+
+             }, 100);
+        }   
+        else if ($(window).scrollTop() <= (wrapper_top)) {
+            $('#email-btn-wrapper').css({
+                'transition': 'none',
+                'top': '32px',
+                'left':'auto',
+                'position':'absolute',
+            });
+            $('.checklist-wrapper').removeClass('scrolled');
             
-            setTimeout(function() {
-                //$('#email-wrapper').addClass('fixed');
-                //$('.email-btn-wrapper').addClass('fixed');
-                //$('#email-wrapper').css({'left':btn_left_minimised, }); 
-                //$('.email-btn-wrapper').css('background-color', 'aquamarine');
-            }, 1000);
-        } else if ($(window).scrollTop() < wrapper_top) {
-            $('#email-wrapper').removeClass('minimised');
-             setTimeout(function() {
-                //$('#email-wrapper').removeClass('fixed');
-               // $('#email-wrapper').css({'left':btn_left_minimised}); 
-            }, 1000);
-        }
+            /*$('#email-btn-wrapper').css('transition','right .05s ease');
+            $('#email-btn-wrapper').removeClass('minimised');*/
+            
+            setTimeout(function(){
+                $('#email-btn-wrapper').css('transition','all .05s ease');
+                $('#email-btn-wrapper').removeClass('minimised');
+            }, 100);
+        } 
     });
-    
-    
-    
-    //https://stackoverflow.com/questions/2202749/jquery-can-i-animate-the-positionabsolute-to-positionrelative
-    
-    
     
     
     /*$(window).resize(function(){
@@ -105,7 +138,7 @@ $(document).ready(function () {
     
     /*------------------- Modal functionality -------------------*/
     // Open modals
-    $('.email-btn-wrapper').on('click', function(){
+    $('#email-btn-wrapper').on('click', function(){
         $('.modal-email-form').addClass('open');
     });
     $('#send-email .combo-button').on('click', function(){
