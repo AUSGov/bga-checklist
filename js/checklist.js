76c9f65630bc58d8e1c0c3c66a5477df885c2e62
blue-baron <bluebaron75@gmail.com>
2020-04-20 18:08:36 +1000

    Mobile responsiveness

--- js/checklist.js
+++ js/checklist.js
@@ -63,77 +63,120 @@ $(document).ready(function () {
     
     /*------------------- Email button functionality -------------------*/
     //Get position of button using the header height
-    var wrapper_pos = $('.checklist-wrapper').offset(),
-        wrapper_top = wrapper_pos.top,
-        btn_pos = $('#email-btn-wrapper').offset(), //calculate so btn can return to original pos
-        btn_left = btn_pos.left,
-        btn_left_min = btn_left + 200 + 32; //left pos + btn width(200px) + gutter(32px)
-        
-     
-    console.log('Checklist top position: ', wrapper_top);
-    console.log('Button left position: ', btn_left);
-    console.log('Button left position - minimised: ', btn_left_min);
+    
+    // ORIGINAL CONCEPT
+    if ($('.checklist-wrapper').hasClass('old')) {
+        var wrapper_pos = $('.checklist-wrapper').offset(),
+            wrapper_top = wrapper_pos.top,
+            btn_pos = $('#email-btn-wrapper').offset(), //calculate so btn can return to original pos
+            btn_left = btn_pos.left,
+            btn_left_min = btn_left + 200 + 32; //left pos + btn width(200px) + gutter(32px)
 
-    
-    $(window).scroll(function(){
-         if ($(window).scrollTop() > (wrapper_top)) {
-             $('#email-btn-wrapper').addClass('minimised');
-             $('.checklist-wrapper').addClass('scrolled');
-             
-             /*$('#email-btn-wrapper').css({
-                     //'transition': 'none',
-                     'top': '32px',
-                     'left': btn_left_min,
-                     'position': 'fixed',
-                 });*/
-             
-             setTimeout(function () {
-                 $('#email-btn-wrapper').css({
-                     'transition': 'none',
-                     'top': '32px',
-                     'left': btn_left_min,
-                     'position': 'fixed',
-                 });
+        console.log('Checklist top position: ', wrapper_top);
+        console.log('Button left position: ', btn_left);
+        console.log('Button left position - minimised: ', btn_left_min);
 
-             }, 100);
-        }   
-        else if ($(window).scrollTop() <= (wrapper_top)) {
-            $('#email-btn-wrapper').css({
-                'transition': 'none',
-                'top': '32px',
-                'left':'auto',
-                'position':'absolute',
-            });
-            $('.checklist-wrapper').removeClass('scrolled');
-            
-            /*$('#email-btn-wrapper').css('transition','right .05s ease');
-            $('#email-btn-wrapper').removeClass('minimised');*/
-            
-            setTimeout(function(){
-                $('#email-btn-wrapper').css('transition','all .05s ease');
-                $('#email-btn-wrapper').removeClass('minimised');
-            }, 100);
-        } 
-    });
-    
-    
-    /*$(window).resize(function(){
-        var checklist_pos = $('.checklist-item:first-of-type').position(),
-            checklist_top = checklist_pos.top;
-            //header_height = $('#page-header').height(),
-            //btn_top = header_top + header_height;
-        //console.log(header_top);
-        console.log(checklist_pos);
-        console.log(checklist_top);
-        
-        $(window).scroll(function () {
-            if($(window).scrollTop() >= (checklist_top)) {
-                $('#email-wrapper').addClass('minimised');
-            } else if ($(window).scrollTop() < checklist_top) {
-                $('#email-wrapper').removeClass('minimised');
-            }
+
+        $(window).scroll(function(){
+             if ($(window).scrollTop() > (wrapper_top)) {
+                 $('#email-btn-wrapper').addClass('minimised');
+                 $('.checklist-wrapper').addClass('scrolled');
+
+                 setTimeout(function () {
+                     $('#email-btn-wrapper').css({
+                         'transition': 'none',
+                         'top': '32px',
+                         'left': btn_left_min,
+                         'position': 'fixed',
+                     });
+
+                 }, 100);
+            }   
+            else if ($(window).scrollTop() <= (wrapper_top)) {
+                $('#email-btn-wrapper').css({
+                    'transition': 'none',
+                    'top': '32px',
+                    'left':'auto',
+                    'position':'absolute',
+                });
+                $('.checklist-wrapper').removeClass('scrolled');
+
+                setTimeout(function(){
+                    $('#email-btn-wrapper').css('transition','width 1s ease');
+                    $('#email-btn-wrapper').removeClass('minimised');
+                }, 100);
+            } 
         });
-    });*/
+
+    }
+    // REVISED CONCEPT
+    if ($('.checklist-wrapper').hasClass('new')) {
+        
+        var wrapper_pos = $('.checklist-wrapper').offset(),
+            wrapper_top = wrapper_pos.top,
+            btn_pos = $('#email-btn-wrapper').offset(), //calculate so btn can return to original pos
+            btn_left = btn_pos.left;
+        
+        var mq = window.matchMedia( "(max-width: 991px)" );
+        if (mq.matches) {
+        
+            $(window).scroll(function(){
+                if ($(window).scrollTop() > (wrapper_top)) {
+                    $('#email-btn-wrapper').addClass('minimised');
+                    $('.checklist-wrapper').addClass('scrolled');
+                    
+                    setTimeout(function () {
+                     $('#email-btn-wrapper').css({
+                         'right': '30px',
+                         'position': 'fixed',
+                     });
+
+                 }, 100);
+                } else if ($(window).scrollTop() <= (wrapper_top)) {
+                    $('#email-btn-wrapper').css({
+                        'right':'0',
+                        'position':'absolute',
+                    });
+                    $('.checklist-wrapper').removeClass('scrolled');
+                    
+                    setTimeout(function(){
+                        $('#email-btn-wrapper').removeClass('minimised');
+                }, 100);
+                }
+            });
+            
+        } else {
+            $(window).scroll(function(){
+                
+                if ($(window).scrollTop() > (wrapper_top)) {
+                     
+                   $('#email-btn-wrapper').css({
+                        'top': '32px',
+                        'left': btn_left,
+                        'right':'auto',
+                        'position': 'fixed',
+                     });
+                    
+                    
+                } else if ($(window).scrollTop() <= (wrapper_top)) {
+                    
+                    $('#email-btn-wrapper').css({
+                        'right':'-224px',
+                        'left':'auto',
+                        'position':'absolute',
+                    });
+                   
+                }
+            });
+            
+            
+        }
+        
+        
+        
+        
+    }
+    
     
     
     /*------------------- Modal functionality -------------------*/
